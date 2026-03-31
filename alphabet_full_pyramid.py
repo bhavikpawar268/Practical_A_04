5e48d37b903c9ee0b4943fd279f15b429ef337de
bhavikpawar268 <bhavikpawar268@gmail.com>
2026-03-31 08:34:41 +0530

    Update alphabet_full_pyramid.py

--- alphabet_full_pyramid.py
+++ alphabet_full_pyramid.py
@@ -1,9 +1,24 @@
-# Full Pyramid using Alphabets
+rows = 5
 
-letters = ["A", "B", "C", "D", "E"]
-
-for i in range(len(letters), 0, -1):
-    print(" " * (len(letters) - i), end="")
+for i in range(1, rows + 1):
+    
+    # Print spaces (for alignment)
+    print(" " * (rows - i), end="")
+    
+    # Print stars
     for j in range(i):
-        print(letters[j], end=" ")
+        print("* ", end="")
+    
+    print()
+# Practical 4B
+rows = 5
+
+for i in range(rows):
+    # Print leading spaces
+    print(" " * i, end="")
+    
+    # Print letters
+    for j in range(rows - i):
+        print(chr(65 + j), end=" ")
+    
     print()
