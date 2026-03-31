c1066e861806bee1319c5dfc5f2e4fd7eff30d74
bhavikpawar268 <bhavikpawar268@gmail.com>
2026-03-31 08:33:38 +0530

    Update inverted_number_pyramid.py

--- inverted_number_pyramid.py
+++ inverted_number_pyramid.py
@@ -1,12 +1,21 @@
-# Inverted Right Half Pyramid using numbers
-
-num = 1
+# Inverted Right Half Pyramid
+# Updated by shvet
 rows = 5
 
-for i in range(1, rows + 1):
+for i in range(rows, 0, -1):
     for j in range(i):
+        print("*", end=" ")
+    print()
+# Practical 4B code 
+num = 1
+
+for i in range(1, 6):      
+    for j in range(i):     
         print(num, end=" ")
+        
         num += 1
-        if num > 9:
+        
+        if num == 10:      
             num = 1
+            
     print()
